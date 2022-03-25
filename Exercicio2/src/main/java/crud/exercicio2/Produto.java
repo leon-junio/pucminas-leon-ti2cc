38c24d349c99f1bfbe0fbfca59c724e9288bb0ee
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-03-24 22:08:43 -0300

    Objeto de produto concluido

--- Exercicio2/src/main/java/crud/exercicio2/Produto.java
+++ Exercicio2/src/main/java/crud/exercicio2/Produto.java
@@ -3,7 +3,7 @@ package crud.exercicio2;
 public class Produto {
 	public Produto() {
 	}
-	
+
 	public Produto(int codigo, String nome, String descricao, float preco, String categoria) {
 		this.codigo = codigo;
 		this.nome = nome;
@@ -11,49 +11,71 @@ public class Produto {
 		this.preco = preco;
 		this.categoria = categoria;
 	}
-	
+
 	private int codigo;
-	private String nome, descricao,categoria;
+	private String nome, descricao, categoria;
 	private float preco;
-	
-	
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNome() {
 		return nome;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+
+	public void setNome(String nome) throws RuntimeException {
+		if (nome.length() == 0) {
+			throw new RuntimeException("O campo informado esta vazio");
+		} else {
+			this.nome = nome;
+		}
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+
+	public void setDescricao(String descricao) throws RuntimeException {
+		if (descricao.length() == 0) {
+			throw new RuntimeException("O campo informado esta vazio");
+		} else {
+			this.descricao = descricao;
+		}
 	}
+
 	public String getCategoria() {
 		return categoria;
 	}
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
+
+	public void setCategoria(String categoria) throws RuntimeException {
+		if (categoria.length() == 0) {
+			throw new RuntimeException("O campo informado esta vazio");
+		} else {
+			this.categoria = categoria;
+		}
 	}
+
 	public float getPreco() {
 		return preco;
 	}
-	public void setPreco(float preco) {
-		this.preco = preco;
+
+	public void setPreco(float preco) throws RuntimeException {
+		if (preco > 0) {
+			this.preco = preco;
+		} else {
+			throw new RuntimeException("Valor menor que 0");
+		}
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Produto [codigo=" + codigo + ", nome=" + nome + ", descricao=" + descricao + ", categoria="
-				+ categoria + ", preco=" + preco + "]";
+		return "Produto [codigo=" + codigo + ", nome=" + nome + ", descricao=" + descricao + ", categoria=" + categoria
+				+ ", preco=" + preco + "]";
 	}
-	
-	
 
 }
